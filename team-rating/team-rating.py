2ad4d8f2ff5a5ebf24311e0c231400517f5c69a0
nickmaher84 <nickmaher84@gmail.com>
2017-05-11 16:22:43 +0100

    Set up get_data to take different numbers of variables per team

--- team-rating/team-rating.py
+++ team-rating/team-rating.py
@@ -4,7 +4,9 @@ import patsy
 import statsmodels.api
 
 
-def get_data(season, division):
+def get_data(season, division, variables=1):
+    assert isinstance(variables, int) and variables in [1, 2, 3, 4]
+
     url = 'https://api.project-hanoi.co.uk/football-data/v1/{0}/{1}'.format(season, division)
     response = requests.get(url)
 
@@ -22,12 +24,27 @@ def get_data(season, division):
 
     combined = home.append(away, ignore_index=True)
 
-    return patsy.dmatrices('Goals ~ Home + Attack + Defend - 1', data=combined, return_type='dataframe')
+    if variables == 1:
+        """One variable per team, plus a global home advantage term"""
+        pass
+
+    elif variables == 2:
+        """Two variables per team - attack and defend - plus a global home advantage term"""
+
+        return patsy.dmatrices('Goals ~ Home + Attack + Defend - 1', data=combined, return_type='dataframe')
+
+    elif variables == 3:
+        """Three variables per team for attack, defend and home advantage"""
+        pass
+
+    elif variables == 4:
+        """Four variables per team, with attack and defend, with and without home advantage"""
+        pass
 
 
 def run_model(season='latest', division='E0'):
     poisson = statsmodels.api.families.Poisson()
-    goals, teams = get_data(season, division)
+    goals, teams = get_data(season, division, variables=2)
 
     model = statsmodels.api.GLM(goals, teams, poisson)
     result = model.fit()
