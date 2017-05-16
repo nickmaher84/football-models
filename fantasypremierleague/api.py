abec2a7a9a10832ef05bf6bdadcce98a2ac31547
nickmaher84 <nickmaher84@gmail.com>
2017-05-16 17:06:44 +0100

    Add Fantasy Premier League code from website project

---
+++ fantasypremierleague/api.py
@@ -0,0 +1,123 @@
+from requests import Session
+from requests.exceptions import Timeout
+from datetime import datetime
+import re
+
+
+class FantasyPremierLeague:
+    def __init__(self, username=None, password=None, verbose=False):
+        self.session = Session()
+        self.username = username
+        self.password = password
+        self.verbose = verbose
+
+    def login(self, username, password):
+        url = 'https://users.premierleague.com/'
+
+        response = self.session.get(url)
+        response.raise_for_status()
+
+        print('Logging in...')
+        token = response.cookies.get('csrftoken')
+        data = {
+            'csrfmiddlewaretoken': token,
+            'login': username,
+            'password': password,
+            'app': 'plusers',
+            'redirect_uri': url
+        }
+        response = self.session.post(url + 'accounts/login/', data=data)
+        response.raise_for_status()
+
+        if self.logged_in():
+            print('Logged in.')
+        else:
+            print('Failed to log in!')
+            exit()
+
+        response = self.session.get('https://fantasy.premierleague.com/')
+        response.raise_for_status()
+
+    def logged_in(self):
+        if self.session.cookies.get('pl_profile'):
+            return True
+        else:
+            return False
+
+    def get_endpoint(self, page, item=None, auth=False):
+        if not self.logged_in() and auth:
+            self.login(self.username, self.password)
+
+        url = 'https://fantasy.premierleague.com/drf/{0}'.format(page)
+        if item is not None:
+            url += '/{0}'.format(item)
+
+        try:
+            response = self.session.get(url)
+        except Timeout:
+            print('Timeout. Retrying...')
+            response = self.session.get(url)
+        if self.verbose:
+            print(response.url)
+        response.raise_for_status()
+
+        return convert(response.json())
+
+    def events(self):
+        return self.get_endpoint('events')
+
+    def players(self):
+        return self.get_endpoint('elements')
+
+    def player(self, player_id):
+        return self.get_endpoint('element-summary', player_id)
+
+    def positions(self):
+        return self.get_endpoint('element-types')
+
+    def fixtures(self):
+        return self.get_endpoint('fixtures')
+
+    def teams(self):
+        return self.get_endpoint('teams')
+
+    def regions(self):
+        return self.get_endpoint('regions')
+
+    def entry(self, entry_id):
+        return self.get_endpoint('entry', entry_id)
+
+    def transfers(self):
+        return self.get_endpoint('transfers', auth=True)
+
+    def my_team(self, entry_id):
+        return self.get_endpoint('my-team', entry_id, auth=True)
+
+    def static_data(self):
+        return self.get_endpoint('bootstrap-static')
+
+    def current_status(self):
+        return self.get_endpoint('bootstrap-dynamic', auth=True)
+
+    def all_data(self):
+        return self.get_endpoint('bootstrap', auth=True)
+
+
+def convert(json):
+    if type(json) is str and re.match(r'^-?\d+\.\d+$', json):
+        return float(json)
+    elif type(json) is str and re.match(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$', json):
+        return datetime.strptime(json, '%Y-%m-%dT%H:%M:%SZ')
+
+    elif type(json) is dict:
+        for k, v in json.items():
+            if type(v) is str and re.match(r'^-?\d+\.\d+$', v):
+                json[k] = float(v)
+            elif type(v) is str and re.match(r'^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$', v):
+                json[k] = datetime.strptime(v, '%Y-%m-%dT%H:%M:%SZ')
+            elif type(v) is dict:
+                json[k] = convert(v)
+            elif type(v) is list:
+                json[k] = [convert(x) for x in v]
+
+    return json
---
+++ fantasypremierleague/utils.py
@@ -0,0 +1,257 @@
+from fantasypremierleague.api import FantasyPremierLeague
+from math import exp
+from pymongo import MongoClient
+
+db = MongoClient().get_database('fantasy-premier-league')
+
+team_table = db['teams']
+team_table.create_index(keys='id', name='team_id', unique=True)
+team_table.create_index(keys='code', name='team_code', unique=True)
+team_table.create_index(keys='name', name='team_name', unique=True)
+team_table.create_index(keys='short_name', name='team_short_name', unique=True)
+
+position_table = db['positions']
+position_table.create_index(keys='id', name='position_id', unique=True)
+position_table.create_index(keys='singular_name', name='position_singular_name', unique=True)
+position_table.create_index(keys='singular_name_short', name='position_singular_name_short', unique=True)
+
+player_table = db['players']
+player_table.create_index(keys='id', name='player_id', unique=True)
+player_table.create_index(keys='code', name='player_code', unique=True)
+player_table.create_index(keys='web_name', name='player_web_name', unique=False)
+player_table.create_index(keys='team', name='player_team', unique=False)
+player_table.create_index(keys='element_type', name='player_position', unique=False)
+
+my_team_table = db['my-team']
+my_team_table.create_index(keys='position', name='squad_position', unique=True)
+my_team_table.create_index(keys='element', name='squad_player_id', unique=True)
+
+
+def full_refresh(username, password):
+    fpl = FantasyPremierLeague(username, password)
+    print('Getting bootstrap')
+    data = fpl.all_data()
+
+    for team in data['teams']:
+        team_id = team['id']
+
+        team_table.update_one({'id': team_id}, {'$set': team}, upsert=True)
+
+    for position in data['element_types']:
+        position_id = position['id']
+
+        scoring = {**data['game-settings']['game'], **data['game-settings']['element_type'][str(position_id)]}
+        for k, v in scoring.items():
+            if k.startswith('scoring_'):
+                if k.endswith('_limit'):
+                    position.setdefault(k.replace('scoring_', 'limit.').replace('_limit', ''), v)
+                else:
+                    position.setdefault(k.replace('scoring_', 'scoring.'), v)
+
+        position_table.update_one({'id': position_id}, {'$set': position}, upsert=True)
+
+    for player in data['elements']:
+        player_id = player['id']
+        print(player.get('web_name'))
+
+        player_details = fpl.player(player_id)
+        player.update(player_details)
+
+        position = position_table.find_one({'id': player['element_type']})
+        scoring = position['scoring']
+        for k in scoring:
+            if not k.endswith('_limit'):
+                player[k] = sum([result.get(k, 0) for result in player['history']])
+        player['short_play'] = len([result for result in player['history'] if position['limit']['long_play'] > result['minutes'] > 0])
+        player['long_play'] = len([result for result in player['history'] if position['limit']['long_play'] <= result['minutes']])
+        player['participation'] = player['minutes'] / (90 * len(player['history']))
+
+        player.setdefault('p90', dict())
+        for k in scoring:
+            if player['minutes']:
+                player['p90'][k] = 90 * player[k] / player['minutes']
+            else:
+                player['p90'][k] = 0
+
+        player.setdefault('expected', dict())
+        for k, v in scoring.items():
+            player['expected'][k] = player['p90'][k] * v
+            if k == 'goals_conceded':
+                player['expected'][k] /= position['limit']['concede']
+            if k == 'saves':
+                player['expected'][k] /= position['limit']['saves']
+            if k == 'clean_sheets':
+                player['expected'][k] = exp(-player['p90']['goals_conceded']) * v
+        if type(player['chance_of_playing_next_round']) is int:
+            availability = player['chance_of_playing_next_round']/100
+        else:
+            availability = 1
+        player['expected_points'] = player['participation'] * availability * sum(player['expected'].values())
+        player['expected_value'] = player['expected_points'] / player['now_cost']
+
+        player_table.update_one({'id': player_id}, {'$set': player}, upsert=True)
+
+    team_refresh(username, password)
+
+
+def team_refresh(username, password):
+    fpl = FantasyPremierLeague(username, password)
+    print('Getting my team')
+    data = fpl.all_data()
+
+    my_team = fpl.my_team(data['entry']['id'])
+    my_team_table.delete_many({})
+    for pick in my_team['picks']:
+        my_team_table.update_one({'position': pick['position']}, {'$set': pick}, upsert=True)
+
+
+def count_teams(players):
+    teams = dict()
+    for player in players:
+        team = player['team']
+        teams.setdefault(team, 0)
+        teams[team] += 1
+    return teams
+
+
+def get_players(position=None, limit=None, key='expected_value', available_only=True):
+    if available_only:
+        parameters = {'status': 'a'}
+    else:
+        parameters = dict()
+
+    if type(position) is int:
+        parameters['element_type'] = position
+    elif type(position) is str and len(position) == 3:
+        parameters['element_type'] = position_table.find_one({'singular_name_short': position}).get('id')
+    elif type(position) is str:
+        parameters['element_type'] = position_table.find_one({'singular_name': position}).get('id')
+
+    players = player_table.find(parameters).sort(key, -1)
+
+    if limit:
+        return players.limit(limit)
+    else:
+        return players
+
+
+def find_trade(team, budget=0, picks=1):
+    reduce_cost = budget < 0
+    teams = count_teams(team)
+
+    trades = list()
+
+    for pick in team:
+        for player in get_players(pick['element_type']):
+            if player not in team:
+                cost = player['now_cost'] - pick['now_cost']
+                points = player['expected_points'] - pick['expected_points']
+                if reduce_cost:
+                    if cost < 0:
+                        if pick['team'] == player['team'] or teams.get(player['team'], 0) < 3:
+                            trade = (pick, player, cost, points, points / cost)
+                            trades.append(trade)
+
+                else:
+                    if points > 0 and budget >= cost:
+                        if pick['team'] == player['team'] or teams.get(player['team'], 0) < 3:
+                            trade = (pick, player, cost, points, cost / points)
+                            trades.append(trade)
+
+    if trades and picks > 1:
+        # TODO: Two picks
+        pass
+
+    elif trades:
+        trades.sort(key=lambda p: (p[4], -p[3]))
+        for pick, player, cost, points, value in trades[:]:
+            print('{0} -> {1} ({2}, {3})'.format(pick['web_name'], player['web_name'], cost, points))
+        return trades[0]
+
+    else:
+        return None
+
+
+def pick_transfers(budget=0, trades=1, wildcard=False):
+    my_team = my_team_table.find()
+
+    if my_team.count():
+        team = list()
+        team_value = 0
+        for pick in my_team:
+            team_value += pick['selling_price']
+            player = player_table.find_one({'id': pick['element']})
+            team.append(player)
+        team.sort(key=lambda p: (p['element_type'], -p['expected_points']))
+        print('Team value: {0} (Transfer budget: {1})'.format(team_value, budget))
+
+        if wildcard:
+            # TODO: Play wildcard and re-select whole team
+            pass
+
+        elif trades == 1:
+            find_trade(team, budget)
+
+        elif trades >= 2:
+            find_trade(team, budget, 2)
+
+    else:
+        return pick_team()
+
+
+def pick_team(budget=1000):
+    print('Starting budget: {0}'.format(budget))
+
+    # Make starting team: best 2xGKP, 5xDEF, 5xMID, 3xFWD
+    team = list()
+    players = get_players()
+    while len(team) < 15:
+        player = players.next()
+        position = player['element_type']
+        position_count = len([p for p in team if p['element_type'] == position])
+        if position_count >= {1: 2, 2: 5, 3: 5, 4: 3}.get(position):
+            continue
+        if count_teams(team).get(player['team'], 0) >= 3:
+            continue
+        team.append(player)
+
+    team.sort(key=lambda p: (p['element_type'], -p['expected_points']))
+    team_cost = sum([player['now_cost'] for player in team])
+    print(team_cost)
+
+    # Trade down until within budget
+    while team_cost > budget:
+        trade = find_trade(team, budget - team_cost)
+
+        if trade:
+            i = team.index(trade[0])
+            team[i] = trade[1]
+            team_cost = sum([player['now_cost'] for player in team])
+            print(team_cost)
+
+        else:
+            print('No more trades')
+            break
+
+    # Trade up with remaining budget
+    while team_cost <= budget:
+        trade = find_trade(team, budget - team_cost)
+
+        if trade:
+            i = team.index(trade[0])
+            team[i] = trade[1]
+            team_cost = sum([player['now_cost'] for player in team])
+            print(team_cost)
+
+        else:
+            print('No more trades')
+            break
+
+    # List team
+    for p in team:
+        print(p['web_name'])
+
+    return team
+
+if __name__ == "__main__":
+    full_refresh('nickmaher84@googlemail.com', 'anfield')
